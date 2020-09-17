bedb98293366994caae31756513baaab57816768
Sartaj Bhuvaji <31826483+SartajBhuvaji@users.noreply.github.com>
2020-09-17 11:14:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,79 @@
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+#from keras.models import Sequential, load_model
+from werkzeug.utils import secure_filename
+import numpy as np
+
+
+
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
+IMAGE_SIZE = (224, 224)#150,150 old
+UPLOAD_FOLDER = 'uploads'
+#vgg16 = load_model('model/brain.h5')
+#from tensorflow.keras.models import load_model
+#vgg16 = load_model('model/brain.h5')
+from keras.models import load_model
+vgg16 = load_model("model/brain.h5")
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+import cv2
+IMG_SIZE = 224 # image size
+CATEGORIES = ["glioma","meningioma","no_tumor","pituitary"]
+
+def prepare(filepath):
+    img_array = cv2.imread(filepath, cv2.IMREAD_COLOR)  # read in the image, convert to grayscale
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize image to match model's expected sizing
+    return new_array.reshape(1, IMG_SIZE, IMG_SIZE, 3)
+
+def predict(file):
+   # img  = load_img(file, target_size=IMAGE_SIZE)
+    #img = img_to_array(img)/255.0
+    #img = np.expand_dims(img, axis=0)
+    #probs = vgg16.predict(img)[0]
+    prediction = vgg16.predict([prepare(file)])
+    #output = CATEGORIES[int(prediction[0][0])]
+    if np.argmax(prediction)==0:    
+        output="glioma"
+    if np.argmax(prediction)==1:
+        output="meningioma"
+    if np.argmax(prediction)==2:
+        output="no_tumor"
+    if np.argmax(prediction)==3:
+        output="pituitary" 
+    return output
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route("/")
+def template_test():
+    return render_template('home.html', label='', imagesource='file://null')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            output = predict(file_path) #call predict fn
+    return render_template("home.html", label=output, imagesource=file_path)
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+if __name__ == "__main__":
+    app.run(debug=False, threaded=False)
